e2837f119aa745cc12e93fe6e2c09afeb211d8d4
adityag26 <adityag2602@gmail.com>
2024-04-29 22:33:48 -0400

    quality control/filtering code

--- data-creation/annotate.py
+++ data-creation/annotate.py
@@ -27,9 +27,11 @@ for input in compressed_file:
     compressed_list.extend(input["compressed_list"])
 
 labeled_file = {}
+filtered_file = defaultdict(list)
 for count, input in enumerate(zip(inital_list, compressed_list)):
     org = breakup_string(input[0])
     comp = breakup_string(input[1])
+    occur = 0
     prev = 0
     end_index = 0
     org_size = len(org)
@@ -39,9 +41,11 @@ for count, input in enumerate(zip(inital_list, compressed_list)):
         org_set.add(word)
     for word in comp:
         flag = False
+        if word.lower() in org_set or word in org_set:
+            occur = occur + 1
         for i in range(150):
             right = min(org_size-1, prev+i)
-            if (word == org[right]) and not labels[right]:
+            if (word.lower() == org[right].lower()) and not labels[right]:
                 labels[right] = True
                 if right - prev > 75:
                     prev += 75
@@ -50,7 +54,7 @@ for count, input in enumerate(zip(inital_list, compressed_list)):
                 flag = True
                 break
             right = max(0, prev - i)
-            if (word == org[right])  and not labels[right]:
+            if (word.lower() == org[right].lower())  and not labels[right]:
                 labels[right] = True
                 prev = right
                 flag = True
@@ -62,9 +66,14 @@ for count, input in enumerate(zip(inital_list, compressed_list)):
         if labels[index]:
             retained_words.append(word)
     retained_string = " ".join(retained_words)
-    labeled_file[count] = { "original": input[0], "labels": labels, "text_to_keep": retained_string}
-
-json.dump(labeled_file, open("label_word2.json", "w"), indent=4)
+    var_rate = 1 - (occur / len(comp))
+    alignment_gap = (occur / len(org)) - (sum(labels) - org_size)
+    labeled_file[count] = { "original": input[0], "labels": labels, "text_to_keep": retained_string, "variation_rate":var_rate, "alignment_gap":alignment_gap}
+    filtered_file["index"].append(count)
+    filtered_file["variation_rate"].append(var_rate)
+    filtered_file["alignment_gap"].append(alignment_gap)
+json.dump(labeled_file, open("label_word.json", "w"), indent=4)
+json.dump(filtered_file, open("filtered_file.json", "w"))
 
 
 
